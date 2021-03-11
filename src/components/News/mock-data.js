17c26383d64f000ffd94ac95739a2f4c8a038b53
My Kieu <huynhthimykieu9999@gmail.com>
2021-03-11 13:06:30 +0700

    first commit

---
+++ src/components/News/mock-data.js
@@ -0,0 +1,37 @@
+import newsDiscoverItemImg1 from '../../assets/img/news/news-2.png';
+import newsDiscoverItemImg2 from '../../assets/img/news/news-3.png';
+import newsDiscoverItemImg3 from '../../assets/img/news/news-4.png';
+import newsDiscoverItemImg4 from '../../assets/img/news/news-5.png';
+
+export const mockData = [
+    {
+        img: newsDiscoverItemImg1,
+        title: 'It is a long established fact that a reader will be distracted',
+        date: 'OCT 10, 2020',
+    },
+    {
+        img: newsDiscoverItemImg2,
+        title: 'It is a long established fact that a',
+        date: 'MAY 30, 2020',
+    },
+    {
+        img: newsDiscoverItemImg3,
+        title: 'It is a long established fact that a reader will be distracted',
+        date: 'FEB 25, 2021',
+    },
+    {
+        img: newsDiscoverItemImg4,
+        title: 'It is a long established fact that a reader will be distracted',
+        date: 'JAN 12, 2021',
+    },
+    {
+        img: newsDiscoverItemImg2,
+        title: 'It is a long established fact that a',
+        date: 'MARCH 15, 2021',
+    },
+    {
+        img: newsDiscoverItemImg3,
+        title: 'It is a long established fact that a reader will be distracted',
+        date: 'FEB 1, 2021',
+    },
+];
